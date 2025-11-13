7616f537be5a413cd5d777850d9aaf12b32ed415
nimesh870 <nimeshdulal870@gmail.com>
2025-11-13 07:17:51 +0545

    added two more methods

--- main.py
+++ main.py
@@ -10,9 +10,12 @@ class Employee:
     def __str__(self):
         return f"({self.emp_id}) {self.name}  {self.gender} -> {self.post}  {self.email}"
     
-    def task(self, task_id, assigned_to, deadline, task_status = "pending"):
+class Task:
+    
+    def task(self, task_id, assigned_to, task_title, deadline, task_status = "pending"):
         self.task_id = task_id
         self.assigned_to = assigned_to
+        self.task_title = task_title
         self.deadline = deadline
         self.task_status = task_status
         
@@ -22,11 +25,11 @@ class Employee:
 class Office_mgmt_sys:
     def __init__(self):
         self.employees = []
-        self.task = []
+        self.tasks= []
          
-    def add_employee(self):
+    def add_employee(self , name , gender , post , email):
         emp_id = len(self.employees) + 1 # increaments by 1 when new employee is added
-        emp = Employee(emp_id, self.name, self.gender, self.post, self.email)
+        emp = Employee(emp_id, name, gender, post, email)
         self.employees.append(emp)
         print(f"✅ Successfully added the details of {self.name}.")
         
@@ -37,6 +40,21 @@ class Office_mgmt_sys:
         for emp in self.employees:
             print(emp)
             
+    def add_task(self, task_id, task_title, assigned_to, deadline, emp_id):
+        employee = next((e for e in self.employees if e.emp_id == emp_id), None)
+        if not self.employees:
+            print("❌ Employees not found.")
             
-    def add_task(self):
-        pass
+            return 
+        task_id = len(self.tasks) + 1
+        
+        task = Task(task_id, task_title, deadline, employee)
+        self.tasks.append(task)
+        print("✅ Tasks added successfully.")
+        
+    def view_task(self):
+        if not self.tasks:
+            print("❌ NO tasks added yet.")
+            
+        for task in self.tasks:
+            print(task)
